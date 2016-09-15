c8f303f21be48255913fc0d5e335173c36775297
set2k6 <set2k6@gmail.com>
2016-09-15 14:20:57 -0500

    finished django database setup, rest api and began angular file structure

--- sneakrs/sneakrs/settings.py
+++ sneakrs/sneakrs/settings.py
@@ -31,14 +31,21 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'deadstox.apps.DeadstoxConfig',
+    # 'deadstox.apps.DeadstoxConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'deadstox',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
