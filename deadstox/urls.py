51c8b13623f79ef8db88e830a5d1b72d1136429a
set2k6 <set2k6@gmail.com>
2016-09-22 23:17:57 -0500

    styled

--- deadstox/urls.py
+++ deadstox/urls.py
@@ -12,27 +12,4 @@ urlpatterns = [
     url(r'^', include(router.urls)),
 
 ]
-# from django.conf.urls import url
 
-# from . import views
-
-# urlpatterns = [
-    # url(r'^$', views.index, name='index'),
-
-    # url(r'^register/', views.Register.as_view(), name='register'),
-
-    # url(r'^login_user/', views.login_user, name='login_user'),
-
-    # url(r'^login/', views.Login.as_view(), name='login'),
-
-    # url(r'^login/', views.logout_user, name='logout'),
-
-    # url(r'^register_user/', views.register_user, name='register_user'),
-
-#     url(r'^closets/$', views.closetCreate, name='closets_add'),
-
-#     url(r'^sneakers/$', views.sneakerCreate, name='sneaker_add'),
-
-#     url(r'^datetimecurrent/$', views.current_datetime, name='datetimecurrent'),
-
-# ]
